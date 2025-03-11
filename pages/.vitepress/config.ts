c848645ca35590fd6b3ba09b6119f77a1534e6c9
xjm <matrix_2024@foxmail.com>
2025-03-11 13:27:03 +0800

    init

---
+++ pages/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "云逸尘小记",
+  description: "一个前端牛马的技术碎碎念",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '一些小案例', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '练习记录',
+        items: [
+          { text: 'makdown的例子', link: '/markdown-examples' },
+          { text: 'api的例子', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ pages/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "云逸尘小记"
+  text: 保持先进生产力
+  tagline: 记录平时遇到的零碎问题以及最新的前端技术
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
